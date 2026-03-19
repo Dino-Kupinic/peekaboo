8017905810f7f4db4cbd822af4518a320cb564e2
Dino-Kupinic <dinokupinic2006@gmail.com>
2026-03-19 20:13:08 +0100

    feat: add otel to demo

--- apps/demo/main.py
+++ apps/demo/main.py
@@ -1,13 +1,32 @@
+from random import randint
+
 from fastapi import FastAPI
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+provider = TracerProvider()
+provider.add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = FastAPI()
+FastAPIInstrumentor.instrument_app(app)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"healthy": True}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/roll")
+def read_item():
+    return str(roll())
+
+def roll():
+    with tracer.start_as_current_span("roll") as rollspan:
+        result = randint(1,6)
+        rollspan.set_attribute("roll.value" , result)
+        return result
